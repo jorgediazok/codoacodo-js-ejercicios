b902e1923088644fc34948b24a39cc5d961e01d7
Jorge <jorgediazok@gmail.com>
2020-11-15 00:48:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// 1. Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert
+//que diga “Hello World”.
+
+//alert('Hello World');
+
+//2. Escribe un programa de una sola línea que escriba en la pantalla un texto que diga
+//“Hello World” (document.write).
+//document.write('Hello World');
+
+//3.Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar
+
+//document.write(3 + 5);
+//console.log(3 + 5);
+
+//4.Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y
+//escriba un texto que diga “Hola nombreUsuario”
+//let username = prompt('What is your name');
+//document.write('Hola ' + username);
+
+//5. Escribe un programa de tres líneas que pida un número, pida otro número y escriba el
+//resultado de sumar estos dos números.
+//let num1 = prompt(parseFloat('Tell me a number'));
+//let num2 = prompt(parseFloat('Now tell me another number'));
+//document.write(num1 + num2);
