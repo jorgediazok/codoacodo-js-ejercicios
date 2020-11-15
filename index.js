d118ae5f69eb798206c93c1751addcd713f076ab
Jorge <jorgediazok@gmail.com>
2020-11-15 12:44:20 -0300

    todo ok hasta el 17

--- index.js
+++ index.js
@@ -70,6 +70,80 @@ document.write(text.substr(i,1);
 
 //11. Escribe un programa que pida una frase y escriba cuántas de las letras que tiene son
 //vocales
-let text = prompt('Escribe una frase');
-var numeroVocales = text.match(/[aeiou]/gi).length;
-document.write(numeroVocales);
+//let text = prompt('Escribe una frase');
+//var numeroVocales = text.match(/[aeiou]/gi).length;
+//document.write(numeroVocales);
+
+//12. Escribe un programa que pida una frase y escriba cuántas veces aparecen cada una de
+//las vocales FALTA HACER ESTE
+//let text = prompt('Escribe una frase');
+
+//13.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7
+// (sólo hay que comprobar si lo es por uno de los cuatro)
+/*let num1 = prompt('Escribe un número');
+if (num1 % 2 === 0 || num1 % 3 === 0 || num1 % 5 === 0 || num1 % 7 === 0) {
+  document.write('Es divisible por 2, 3, 5 o 7');
+} else {
+  document.write('No es divisible por 2, 3, 5 ni 7');
+}*/
+
+//14.- Añadir al ejercicio anterior que nos diga por cual de los cuatro es
+//divisible (hay que decir todos por los que es divisible)
+/*var num1 = prompt('Escribe un número');
+if (num1 % 2 === 0 || num1 % 3 === 0 || num1 % 5 === 0 || num1 % 7 === 0) {
+  if (num1 % 2 === 0) {
+    document.write('Es divisible por 2. ');
+  }
+  if (num1 % 3 === 0) {
+    document.write('Es divisible por 3. ');
+  }
+  if (num1 % 5 === 0) {
+    document.write('Es divisible por 5. ');
+  }
+  if (num1 % 7 === 0) {
+    document.write('Es divisible por 7. ');
+  }
+} else {
+  document.write('No es divisible ni por 2, ni por 3, ni por 5, ni por 7');
+}*/
+
+//15.- Escribir un programa que escriba en pantalla los divisores de un número dado
+/*let num = prompt('Escribe un número');
+let i;
+for (i = 2; i < num / 2; i++) {
+  if (num % i === 0) {
+    document.write(i, ', ');
+  }
+}*/
+
+//16.- Escribir un programa que escriba en pantalla los divisores comunes de dos números dados
+/*let num1 = prompt('Escribe un número');
+let num2 = prompt('Escribe otro número');
+let menor;
+let i;
+if (num1 < num2) {
+  menor = num1;
+} else {
+  menor = num2;
+}
+for (i = 2; i < menor / 2; i++) {
+  if (num1 % i === 0 && num2 % i === 0) {
+    document.write(i, ', ');
+  }
+}*/
+
+//17.- Escribir un programa que nos diga si un número dado es primo
+//(no es divisible por ninguno otro número que no sea él mismo o la unidad)
+/*let num1 = prompt('Escribe un número');
+let i;
+let numPrimo = true;
+for (i = 2; i < num1 / 2; i++) {
+  if (num1 % i === 0) {
+    numPrimo === false;
+  }
+}
+if (numPrimo) {
+  document.write('el número es primo');
+} else {
+  document.write('el número no es primo');
+}*/
